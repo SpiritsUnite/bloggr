a19af5e6f36f8ad17bba75b459519142b35ff198
Austin Tankiang <austi.tanki@gmail.com>
2014-08-01 19:47:57 +1000

    added sign up tests with valid data

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "Users", :type => :request do
-  describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response.status).to be(200)
+RSpec.describe "User Pages", :type => :request do
+
+  subject { page }
+
+  describe "sign up" do
+    before { visit signup_path }
+
+    it { should have_content("Sign up") }
+
+    context "with valid information" do
+      before do
+        fill_in "Username", :with => "aoeu"
+        fill_in "Email",    :with => "test@example.com"
+        fill_in "Password", :with => "Foobar"
+        fill_in "Password confirmation", :with => "Foobar"
+      end
+
+      it "should add the user to the database" do
+        expect { click_button "Sign up" }.to change(User, :count).by(1)
+      end
+
+      context "after clicking button" do
+        before { click_button "Sign up" }
+        it { should have_content "Success" }
+      end
     end
   end
 end
