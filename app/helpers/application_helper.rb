55099afeb7f1008febfd9e663362fe217df8497b
Austin Tankiang <austi.tanki@gmail.com>
2014-08-03 17:29:24 +1000

    changed nav_link to a helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
+
   def full_title(title)
     base = "Bloggr"
     return base if title.empty?
     return "#{title} | #{base}"
   end
 
+  def nav_link(name, path, options = nil)
+    render "layouts/nav_link", name: name, path: path, options: options
+  end
 end
