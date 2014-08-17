993b71892703c5d70e2a5f84a50b1a33e3fdee79
Austin Tankiang <austi.tanki@gmail.com>
2014-08-17 18:08:32 +1000

    Paginated posts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @posts = Post.where(published: true).order("created_at DESC")
+    @posts = Post.where(published: true).order("created_at DESC").page(params[:page]).per_page(5)
   end
 end
